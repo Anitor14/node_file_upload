a62db3de4ce2d600e7c0e1c3a485ffdd4f3ba5a4
Anitor <anitorabraham@gmail.com>
2022-11-24 15:39:25 +0100

    established the routes and controllers

---
+++ controllers/uploadsController.js
@@ -0,0 +1,9 @@
+const { StatusCodes } = require("http-status-codes");
+
+const uploadProductImage = async (req, res) => {
+  res.send("create Product");
+};
+
+module.exports = {
+  uploadProductImage,
+};
